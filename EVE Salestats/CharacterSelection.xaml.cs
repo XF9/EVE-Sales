37a4c0869ed2e5e4730b50323f9c14691a398b44
Michael Moehler <m.moehler@xf9.de>
2015-10-29 14:08:42 +0100

    fixed character display

--- EVE Salestats/CharacterSelection.xaml.cs
+++ EVE Salestats/CharacterSelection.xaml.cs
@@ -15,8 +15,6 @@ using Windows.UI.Xaml.Navigation;
 
 using EVE_Salestats.Char;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace EVE_Salestats
 {
     /// <summary>
@@ -39,12 +37,12 @@ namespace EVE_Salestats
 
             Grid charGrid = new Grid();
 
-            ColumnDefinition spacer_top = new ColumnDefinition();
-            ColumnDefinition spacer_bottom = new ColumnDefinition();
-            spacer_top.Width = new GridLength(1, GridUnitType.Star);
-            spacer_bottom.Width = new GridLength(1, GridUnitType.Star);
+            ColumnDefinition spacerTop = new ColumnDefinition();
+            ColumnDefinition spacerBottom = new ColumnDefinition();
+            spacerTop.Width = new GridLength(1, GridUnitType.Star);
+            spacerBottom.Width = new GridLength(1, GridUnitType.Star);
 
-            charGrid.ColumnDefinitions.Add(spacer_top);
+            charGrid.ColumnDefinitions.Add(spacerTop);
 
             int columnIndex = 1;
             foreach(Character character in characters){
@@ -55,60 +53,80 @@ namespace EVE_Salestats
                 charGrid.ColumnDefinitions.Add(columnDefinition);
                 
                 // border arround
-                Border border = new Border();
-                border.BorderBrush = color_text;
-                border.BorderThickness = new Thickness(0.5);
-                border.Background = color_background;
-                border.Width = 160;
-                border.Height = 260;
-                border.Margin = new Thickness(20);
-                border.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
-                border.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
-                Grid.SetColumn(border, columnIndex);
-                Grid.SetRow(border, 0);
+                Border characterBox = new Border();
+                characterBox.BorderBrush = color_text;
+                characterBox.BorderThickness = new Thickness(0.5);
+                characterBox.Background = color_background;
+                characterBox.Width = 160;
+                characterBox.Height = 260;
+                characterBox.Margin = new Thickness(10);
+                characterBox.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
+                characterBox.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
+                Grid.SetColumn(characterBox, columnIndex);
+                Grid.SetRow(characterBox, 0);
 
                 // name
                 TextBlock name = new TextBlock();
                 name.Text = character.Name;
                 name.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
-                name.Margin = new Thickness(0, 160, 0, 0);
+                name.Margin = new Thickness(0, 0, 0, 0);
                 name.FontWeight = Windows.UI.Text.FontWeights.Bold;
                 name.FontSize = 15;
-                Grid.SetColumn(name, columnIndex);
-                Grid.SetRow(name, 0);
+                Grid.SetColumn(name, 0);
+                Grid.SetRow(name, 1);
 
-                // name
+                // corp
                 TextBlock corp = new TextBlock();
                 corp.Text = character.Corp;
                 corp.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
-                corp.Margin = new Thickness(0, 190, 0, 0);
+                corp.Margin = new Thickness(0, 0, 0, 0);
                 corp.FontWeight = Windows.UI.Text.FontWeights.Normal;
                 corp.FontSize = 15;
                 corp.TextWrapping = TextWrapping.WrapWholeWords;
-                Grid.SetColumn(corp, columnIndex);
-                Grid.SetRow(corp, 0);
+                corp.Width = 140;
+                corp.TextAlignment = TextAlignment.Center;
+                Grid.SetColumn(corp, 0);
+                Grid.SetRow(corp, 2);
 
-                // name
+                // ballance
                 TextBlock ballance = new TextBlock();
                 ballance.Text = character.Ballance + " ISK";
                 ballance.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
-                ballance.Margin = new Thickness(0, 220, 0, 0);
+                ballance.Margin = new Thickness(0, 0, 0, 0);
                 ballance.FontWeight = Windows.UI.Text.FontWeights.Normal;
                 ballance.FontSize = 15;
                 ballance.TextWrapping = TextWrapping.WrapWholeWords;
-                Grid.SetColumn(ballance, columnIndex);
-                Grid.SetRow(ballance, 0);
+                Grid.SetColumn(ballance, 0);
+                Grid.SetRow(ballance, 3);
 
-                charGrid.Children.Add(border);
-                charGrid.Children.Add(name);
-                charGrid.Children.Add(corp);
-                charGrid.Children.Add(ballance);
+                RowDefinition row_image = new RowDefinition();
+                RowDefinition row_name = new RowDefinition();
+                RowDefinition row_corp = new RowDefinition();
+                RowDefinition row_balance = new RowDefinition();
+                RowDefinition row_space = new RowDefinition();
+                row_image.Height = new GridLength(160, GridUnitType.Pixel);
+                row_name.Height = GridLength.Auto;
+                row_corp.Height = GridLength.Auto;
+                row_balance.Height = GridLength.Auto;
+                row_space.Height = new GridLength(1, GridUnitType.Star);
+                Grid characterData = new Grid();
+                characterData.RowDefinitions.Add(row_image);
+                characterData.RowDefinitions.Add(row_name);
+                characterData.RowDefinitions.Add(row_corp);
+                characterData.RowDefinitions.Add(row_balance);
+                characterData.RowDefinitions.Add(row_space);
+
+                charGrid.Children.Add(characterBox);
+                characterBox.Child = characterData;
+                characterData.Children.Add(name);
+                characterData.Children.Add(corp);
+                characterData.Children.Add(ballance);
                 columnIndex++;
 
                 System.Diagnostics.Debug.WriteLine(character.Ballance);
             }
 
-            charGrid.ColumnDefinitions.Add(spacer_bottom);
+            charGrid.ColumnDefinitions.Add(spacerBottom);
 
             this.MainGrid.Children.Add(charGrid);
             Grid.SetRow(charGrid, 2);
