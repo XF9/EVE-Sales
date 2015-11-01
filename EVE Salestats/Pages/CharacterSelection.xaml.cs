0f79b19e45fd08914a0fd5e3e9160df4e6abc8c1
Michael Moehler <m.moehler@xf9.de>
2015-11-01 08:57:59 +0100

    added templates to clean up  code

--- EVE Salestats/Pages/CharacterSelection.xaml.cs
+++ EVE Salestats/Pages/CharacterSelection.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 using EVE_Salestats.Char;
+using EVE_Salestats.Templates;
 
 namespace EVE_Salestats.Pages
 {
@@ -30,7 +31,6 @@ namespace EVE_Salestats.Pages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             Character[] characters = e.Parameter as Character[];
-            Grid charGrid = new Grid();
 
             int index = 0;
             foreach (Character character in characters)
@@ -38,89 +38,14 @@ namespace EVE_Salestats.Pages
                 // create a field for every character
                 RowDefinition rowDefinition = new RowDefinition();
                 rowDefinition.Height = Windows.UI.Xaml.GridLength.Auto;
-                charGrid.RowDefinitions.Add(rowDefinition);
+                this.CharGrid.RowDefinitions.Add(rowDefinition);
 
-                // char button
-                Button characterBox = new Button();
-                //characterBox.MinHeight = 148;
-                characterBox.Margin = new Thickness(0);
-                characterBox.BorderThickness = new Thickness(0.5);
-                characterBox.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
-                characterBox.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
-                Grid.SetColumn(characterBox, 0);
-                Grid.SetRow(characterBox, index++);
+                CharacterDisplay charInfo = new CharacterDisplay(character.Name, character.Corp + "\n" + character.Ballance.ToString("n", EVE_Salestats.Settings.numberFormat) + " ISK", character.Image);
+                Grid.SetRow(charInfo, index++);
+                Grid.SetColumn(charInfo, 0);
 
-                // image
-                Image image = new Image();
-                image.Source = character.Image;
-                image.Width = 128;
-                image.Height = 128;
-                image.Margin = new Thickness(0);
-                image.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
-                image.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
-                Grid.SetColumn(image, 0);
-                Grid.SetRowSpan(image, 2);
-                Grid.SetRow(image, 0);
-
-                // name
-                TextBlock name = new TextBlock();
-                name.Text = character.Name;
-                name.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
-                name.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Bottom;
-                name.Margin = new Thickness(10,0,0,0);
-                name.FontWeight = Windows.UI.Text.FontWeights.Bold;
-                name.FontSize = 20;
-                Grid.SetColumn(name, 1);
-                Grid.SetRow(name, 0);
-                
-                // corp
-                TextBlock charInfo = new TextBlock();
-                charInfo.Text = "\n" + character.Corp + "\n" + character.Ballance.ToString("n", EVE_Salestats.Settings.numberFormat) + " ISK";
-                charInfo.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
-                charInfo.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
-                charInfo.Margin = new Thickness(10, 0, 0, 0);
-                charInfo.FontWeight = Windows.UI.Text.FontWeights.Normal;
-                charInfo.FontSize = 15;
-                charInfo.TextWrapping = TextWrapping.WrapWholeWords;
-                charInfo.TextAlignment = TextAlignment.Left;
-                Grid.SetColumn(charInfo, 1);
-                Grid.SetRow(charInfo, 1);
-
-                ColumnDefinition col_image = new ColumnDefinition();
-                ColumnDefinition col_data = new ColumnDefinition();
-
-                RowDefinition row_name = new RowDefinition();
-                RowDefinition row_info = new RowDefinition();
-                RowDefinition row_space = new RowDefinition();
-
-                col_image.Width = new GridLength(128, GridUnitType.Pixel);
-                col_data.Width = new GridLength(1, GridUnitType.Star);
-
-                row_name.Height = GridLength.Auto;
-                row_info.Height = GridLength.Auto;
-                row_space.Height = new GridLength(1, GridUnitType.Star);
-
-                Grid characterData = new Grid();
-                characterData.Width = 350;
-                characterData.MinHeight = 128;
-                characterData.ColumnDefinitions.Add(col_image);
-                characterData.ColumnDefinitions.Add(col_data);
-                characterData.RowDefinitions.Add(row_name);
-                characterData.RowDefinitions.Add(row_info);
-                characterData.RowDefinitions.Add(row_space);
-
-
-                charGrid.Children.Add(characterBox);
-                characterBox.Content = characterData;
-                characterData.Children.Add(image);
-                characterData.Children.Add(name);
-                characterData.Children.Add(charInfo);
-                //characterData.Children.Add(ballance);
+                this.CharGrid.Children.Add(charInfo);
             }
-
-            this.MainGrid.Children.Add(charGrid);
-            Grid.SetRow(charGrid, 2);
-            Grid.SetColumn(charGrid, 1);
         }
 
         private void SelectCharacter(object sender, RoutedEventArgs e)
---
+++ EVE Salestats/Templates/CharacterDisplay.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI.Xaml.Media.Imaging;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace EVE_Salestats.Templates
+{
+    public sealed partial class CharacterDisplay : UserControl
+    {
+        public CharacterDisplay(String name, String description, BitmapImage CharacterImage)
+        {
+            this.InitializeComponent();
+            this.CharacterName.Text = name;
+            this.CharacterDescription.Text = description;
+            this.CharacterImage.Source = CharacterImage;
+        }
+    }
+}
