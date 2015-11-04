6fbc5636ced6a4cf4f4cbd629aefa55505ed34eb
Michael Moehler <m.moehler@xf9.de>
2015-11-04 16:15:55 +0100

    Added average value for sales and buys

--- EVE Salestats/Pages/SaleStats.xaml.cs
+++ EVE Salestats/Pages/SaleStats.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.UI;
+
 using EVE_SaleTools.Entities;
 
 using SQLite;
@@ -42,12 +44,80 @@ namespace EVE_SaleTools.Pages
 
             // load all transaction items
             var sqlite = new SQLiteAsyncConnection(activeCharacter.CharID);
-            List<Transaction> transactions = await sqlite.QueryAsync<Transaction>("SELECT * FROM 'Transaction' GROUP BY TypeID ORDER BY TypeName");
-            this.Itemlist.ItemsSource = transactions;
+
+            List<Transaction> items = await sqlite.QueryAsync<Transaction>("SELECT * FROM 'Transaction' GROUP BY TypeID ORDER BY TypeName");
+            this.Itemlist.ItemsSource = items;
 
             // tmp - query mexallon stats
-            List<Transaction> mexallon = await sqlite.QueryAsync<Transaction>("SELECT Time, AVG(PricePerUnit) AS PricePerUnit FROM 'Transaction' WHERE TypeName = 'Mexallon' AND BuyOrder = 0 GROUP BY strftime('%d-%m-%Y',Time)");
-            (LineChart.Series[0] as LineSeries).ItemsSource = mexallon;
+
+            SolidColorBrush line_sales = new SolidColorBrush(Color.FromArgb(255, 116, 195, 101));
+            Style point_sales = new Style();
+            point_sales.TargetType = typeof(DataPoint);
+            point_sales.Setters.Add(new Setter(BackgroundProperty, line_sales));
+            (LineChart.Series[0] as LineSeries).Background = line_sales;
+            (LineChart.Series[0] as LineSeries).DataPointStyle = point_sales;
+
+            SolidColorBrush line_salesAvg = new SolidColorBrush(Color.FromArgb(255, 34, 139, 34));
+            Style point_salesAvg = new Style();
+            point_salesAvg.TargetType = typeof(DataPoint);
+            point_salesAvg.Setters.Add(new Setter(BackgroundProperty, line_salesAvg));
+            point_salesAvg.Setters.Add(new Setter(WidthProperty, 0));
+            point_salesAvg.Setters.Add(new Setter(HeightProperty, 0));
+            (LineChart.Series[1] as LineSeries).Background = line_salesAvg;
+            (LineChart.Series[1] as LineSeries).DataPointStyle = point_salesAvg;
+
+            SolidColorBrush line_buys = new SolidColorBrush(Color.FromArgb(255, 196, 2, 51));
+            Style point_buys = new Style();
+            point_buys.TargetType = typeof(DataPoint);
+            point_buys.Setters.Add(new Setter(BackgroundProperty, line_buys));
+            (LineChart.Series[2] as LineSeries).Background = line_buys;
+            (LineChart.Series[2] as LineSeries).DataPointStyle = point_buys;
+
+            SolidColorBrush line_buysAvg = new SolidColorBrush(Color.FromArgb(255, 237, 28, 36));
+            Style point_buysAvg = new Style();
+            point_buysAvg.TargetType = typeof(DataPoint);
+            point_buysAvg.Setters.Add(new Setter(BackgroundProperty, line_buysAvg));
+            point_buysAvg.Setters.Add(new Setter(WidthProperty, 0));
+            point_buysAvg.Setters.Add(new Setter(HeightProperty, 0));
+            (LineChart.Series[3] as LineSeries).Background = line_buysAvg;
+            (LineChart.Series[3] as LineSeries).DataPointStyle = point_buysAvg;
+
+
+
+            List<Transaction> mexallonSalesPerDay = await sqlite.QueryAsync<Transaction>("SELECT Time, AVG(PricePerUnit) AS PricePerUnit FROM 'Transaction' WHERE TypeName = 'Mexallon' AND BuyOrder = 0 GROUP BY strftime('%d-%m-%Y',Time) ORDER BY Time ASC");
+
+            if (mexallonSalesPerDay.Count > 0)
+            {
+                LineSeries sales = new LineSeries();
+                (LineChart.Series[0] as LineSeries).ItemsSource = mexallonSalesPerDay;
+
+                List<Transaction> mexallonSalesAveragePrice = await sqlite.QueryAsync<Transaction>("SELECT Time, AVG(PricePerUnit) AS PricePerUnit FROM 'Transaction' WHERE TypeName = 'Mexallon' AND BuyOrder = 0 GROUP BY TypeName");
+                Transaction begin = new Transaction();
+                begin.Time = mexallonSalesPerDay.Last().Time;
+                begin.PricePerUnit = mexallonSalesAveragePrice.First().PricePerUnit;
+                Transaction end = new Transaction();
+                end.Time = mexallonSalesPerDay.First().Time;
+                end.PricePerUnit = mexallonSalesAveragePrice.First().PricePerUnit;
+                (LineChart.Series[1] as LineSeries).ItemsSource = new List<Transaction>() { begin, end };
+            }
+
+
+            List<Transaction> mexallonBuysPerDay = await sqlite.QueryAsync<Transaction>("SELECT Time, AVG(PricePerUnit) AS PricePerUnit FROM 'Transaction' WHERE TypeName = 'Mexallon' AND BuyOrder = 1 GROUP BY strftime('%d-%m-%Y',Time) ORDER BY Time ASC");
+            if (mexallonBuysPerDay.Count > 0)
+            {
+                (LineChart.Series[0] as LineSeries).ItemsSource = mexallonBuysPerDay;
+
+
+                List<Transaction> mexallonBuysAveragePrice = await sqlite.QueryAsync<Transaction>("SELECT Time, AVG(PricePerUnit) AS PricePerUnit FROM 'Transaction' WHERE TypeName = 'Mexallon' AND BuyOrder = 1 GROUP BY TypeName");
+                Transaction begin = new Transaction();
+                begin.Time = mexallonBuysPerDay.Last().Time;
+                begin.PricePerUnit = mexallonBuysPerDay.First().PricePerUnit;
+                Transaction end = new Transaction();
+                end.Time = mexallonSalesPerDay.First().Time;
+                end.PricePerUnit = mexallonBuysPerDay.First().PricePerUnit;
+                (LineChart.Series[1] as LineSeries).ItemsSource = new List<Transaction>() { begin, end };
+
+            }
         }
     }
 }
